6968624e7a63268cec60c6aa45242589b3c3eb05
djimderidder <36667917+djimderidder@users.noreply.github.com>
2019-11-01 18:28:46 +0100

    fixed code

--- diamondSquare.m
+++ diamondSquare.m
@@ -2,7 +2,7 @@ function [surface] = diamondSquare(n,h,irange)
 %Diamond square method
 %   Detailed explanation goes here
     N=(2^n)+1;
-    S = ones((2*N)-1,(2*N)-1);
+    S = zeros((2*N)-1,(2*N)-1);
     for i=1:N
         for j=1:N
             x=(N-1)/2+i;
@@ -20,6 +20,13 @@ function [surface] = diamondSquare(n,h,irange)
                 x1 = ((j-1)*x)+e;
                 y1 = ((k-1)*x)+e;
                 S(x1+d,y1+d) = sumRand(x1+d,y1+d,d,S,range);
+            end
+        end
+%         surface = S(e:N+e-1,e:N+e-1);subplot(2,4,2*i-1); heatmap(surface);
+        for j=1:2^(i-1)
+            for k=1:2^(i-1)
+                x1 = ((j-1)*x)+e;
+                y1 = ((k-1)*x)+e;
                 if S(x1+d,y1)==1
                     S(x1+d,y1) = sumRand(x1+d,y1,d,S,range);
                 end
@@ -34,11 +41,15 @@ function [surface] = diamondSquare(n,h,irange)
                 end
             end
         end
-        range = range*h*2^(-h);
+%         surface = S(e:N+e-1,e:N+e-1);subplot(2,4,2*i); heatmap(surface);
+        range = range*2^(-h);
     end
 
     surface = S(e:N+e-1,e:N+e-1);
     figure()
     surf(surface)
     axis([0 size(surface,1) 0 size(surface,1) -size(surface,1) size(surface,1)])
-    title(['n = ',num2str(n),'; h = ',num2str(h)])
+    title(['n = ',num2str(n),'; h = ',num2str(h),'; initial range RV = ',num2str(irange)]);
+    ax = gca;
+    ax.TitleFontSizeMultiplier = 4;
+end
